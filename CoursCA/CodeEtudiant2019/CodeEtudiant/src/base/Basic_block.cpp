7ab6875d2320574b7a5377d0e5eaddb8de75fc05
KatiaAmichi <3603567@ppti-14-507-05.ufr-info-p6.jussieu.fr>
2019-04-18 20:01:13 +0200

    implement TP3-2, not sure of the results

--- CoursCA/CodeEtudiant2019/CodeEtudiant/src/base/Basic_block.cpp
+++ CoursCA/CodeEtudiant2019/CodeEtudiant/src/base/Basic_block.cpp
@@ -3,32 +3,32 @@
 
 
 void Basic_block::show_dependances(Instruction *i1, Instruction *i2){
-   
-   if(i1->is_dep_RAW1(i2)) 
-      cout<<"Dependance i"<<i1->get_index()<<"->i"<<i2->get_index()<<": RAW1"<<endl; 
-   if(i1->is_dep_RAW2(i2)) 
+
+   if(i1->is_dep_RAW1(i2))
+      cout<<"Dependance i"<<i1->get_index()<<"->i"<<i2->get_index()<<": RAW1"<<endl;
+   if(i1->is_dep_RAW2(i2))
       cout<<"Dependance i"<<i1->get_index()<<"->i"<<i2->get_index()<<": RAW2"<<endl;
-   
-   if(i1->is_dep_WAR1(i2)) 
+
+   if(i1->is_dep_WAR1(i2))
       cout<<"Dependance i"<<i1->get_index()<<"->i"<<i2->get_index()<<": WAR1"<<endl;
-  
-   if(i1->is_dep_WAR2(i2)) 
+
+   if(i1->is_dep_WAR2(i2))
       cout<<"Dependance i"<<i1->get_index()<<"->i"<<i2->get_index()<<": WAR2"<<endl;
-   
-   if(i1->is_dep_WAW(i2)) 
+
+   if(i1->is_dep_WAW(i2))
       cout<<"Dependance i"<<i1->get_index()<<"->i"<<i2->get_index()<<": WAW"<<endl;
-   
-   if(i1->is_dep_MEM(i2)) 
+
+   if(i1->is_dep_MEM(i2))
       cout<<"Dependance i"<<i1->get_index()<<"->i"<<i2->get_index()<<": MEM"<<endl;
-   
+
 }
 
 Basic_block::Basic_block():
   Use(vector<bool>(NB_REG, false)),
   Def(vector<bool>(NB_REG, false)),
   LiveIn(vector<bool>(NB_REG, false)),
-  LiveOut(vector<bool>(NB_REG, false)), 
-  DefLiveOut(vector<int>(NB_REG, -1)), 
+  LiveOut(vector<bool>(NB_REG, false)),
+  DefLiveOut(vector<int>(NB_REG, -1)),
   Domin(vector<bool>(NB_MAX_BB, true)){
    _head = nullptr;
    _end = nullptr;
@@ -87,7 +87,7 @@ Basic_block *Basic_block::get_successor(int index){
 	 i--;
      }
    }
-   else cout<<"Error Basic_block::get_successor: index is bigger than the size of the list"<<endl; 	
+   else cout<<"Error Basic_block::get_successor: index is bigger than the size of the list"<<endl;
    return _succ.back();
 }
 
@@ -107,9 +107,9 @@ Basic_block *Basic_block::get_predecessor(int index){
 	 i--;
      }
    }
-   else cout<<"Error Basic_block::get_predecessor: index is bigger than the size of the list"<<endl; 	
+   else cout<<"Error Basic_block::get_predecessor: index is bigger than the size of the list"<<endl;
    return _pred.back();
-	
+
 }
 
 int Basic_block::get_nb_succ(){
@@ -132,9 +132,9 @@ void Basic_block::display(){
   cout<<"Begin BB"<< get_index() << endl;
    Line* element = _head;
    int i=0;
-   if(element == _end)	
+   if(element == _end)
       cout << _head->get_content() <<endl;
-  
+
    while(element != _end->get_next()){
       if(element->isInst()){
 	 cout<<"i"<<i<<" ";
@@ -142,7 +142,7 @@ void Basic_block::display(){
       }
       if(!element->isDirective())
 	 cout <<element->get_content() <<endl;
-      
+
       element = element->get_next();
    }
    cout<<"End BB"<< get_index() << endl;
@@ -151,17 +151,17 @@ void Basic_block::display(){
 string Basic_block::get_content(){
    string rt = "";
    Line* element = _head;
-   
+
    while(element != _end->get_next()){
      if(element->isInst()){
-	rt = rt + element->get_content() + "\\l" ;	 	
+	rt = rt + element->get_content() + "\\l" ;
      }
      else if(element->isLabel())
        rt = rt + element->get_content() + "\\l" ;
-      
+
       element = element->get_next();
    }
-  
+
    return rt ;
 }
 
@@ -170,31 +170,31 @@ int Basic_block::size(){
    int lenght=0;
    while(element != _end){
       lenght++;
-      if(element->get_next()==_end)	
+      if(element->get_next()==_end)
 	 break;
-      else 
+      else
 	 element = element->get_next();
    }
    return lenght;
-}	
+}
 
 
-void Basic_block::restitution(string const filename){	
+void Basic_block::restitution(string const filename){
    Line* element = _head;
    ofstream monflux(filename.c_str(), ios::app);
    if(monflux){
      monflux<<"Begin BB"<< get_index() << endl;
-      if(element == _end)	
+      if(element == _end)
 	monflux << _head->get_content() <<endl;
       while(element != _end)
       {
-	 if(element->isInst()) 
+	 if(element->isInst())
 	    monflux<<"\t";
 	 if(!element->isDirective())
 	    monflux << element->get_content()<<endl ;
-		
+
 	 if(element->get_next()==_end){
-	    if(element->get_next()->isInst()) 
+	    if(element->get_next()->isInst())
 	       monflux<<"\t";
 	    if(!element->isDirective())
 	       monflux << element->get_next()->get_content()<<endl;
@@ -202,7 +202,7 @@ void Basic_block::restitution(string const filename){
 	 }
 	 else element = element->get_next();
       }
-      monflux<<"End BB" << get_index() << "\n\n"<<endl;		
+      monflux<<"End BB" << get_index() << "\n\n"<<endl;
    }
    else {
       cout<<"Error cannot open the file"<<endl;
@@ -218,27 +218,27 @@ bool Basic_block::is_labeled(){
    else return false;
 }
 
-int Basic_block::get_nb_inst(){   
+int Basic_block::get_nb_inst(){
    if (_nb_instr == 0)
       link_instructions();
    return _nb_instr;
-    
+
 }
 
 Instruction* Basic_block::get_instruction_at_index(int index){
    Instruction *inst;
-   
+
    if(index >= get_nb_inst()){
       return nullptr;
-   }   
+   }
    inst=get_first_instruction();
    for(int i=0; i<index; i++, inst=inst->get_next());
-   
+
    return inst;
 }
 
 Line* Basic_block::get_first_line_instruction(){
-   
+
    Line *current = _head;
    while(!current->isInst()){
       current=current->get_next();
@@ -278,10 +278,10 @@ void Basic_block::link_instructions(){
    i1->set_index(index);
    index++;
    Instruction *i2;
-   
+
 //Calcul des successeurs
    while(current != _end){
-   
+
       while(!next->isInst()){
 	 next=next->get_next();
 	 if(next==_end){
@@ -294,12 +294,12 @@ void Basic_block::link_instructions(){
 	    }
 	 }
       }
-      
+
       i2 = getInst(next);
       i2->set_index(index);
       index++;
       i1->set_link_succ_pred(i2);
-      
+
       i1=i2;
       current=next;
       next=next->get_next();
@@ -335,7 +335,7 @@ void add_dep_link(Instruction *pred, Instruction* succ, t_Dep type){
    d->inst=succ;
    d->type=type;
    pred->add_succ_dep(d);
-   
+
    d=(dep*)malloc(sizeof(dep));
    d->inst=pred;
    d->type=type;
@@ -345,7 +345,7 @@ void add_dep_link(Instruction *pred, Instruction* succ, t_Dep type){
 
 /* calcul des d�pendances entre les instructions dans le bloc  */
 /* une instruction a au plus 1 reg dest et 2 reg sources */
-/* Attention le reg source peut �tre 2 fois le m�me */ 
+/* Attention le reg source peut �tre 2 fois le m�me */
 /* Utiliser les m�thodes is_dep_RAW1, is_dep_RAW2, is_dep_WAR1, is_dep_WAR2, is_dep_WAW, is_dep_MEM pour d�terminer les d�pendances */
 
 /* ne pas oublier les d�pendances de controle avec le branchement s'il y en a un */
@@ -354,10 +354,10 @@ void add_dep_link(Instruction *pred, Instruction* succ, t_Dep type){
 
 
 void Basic_block::comput_pred_succ_dep(){
-   
+
   link_instructions(); // essentiel pour avoir un lien entre les instructions
    if (dep_done) return;
-  
+
    /* A REMPLIR */
    if(!dep_done){
 	   Instruction * current_inst = get_last_instruction();
@@ -419,8 +419,8 @@ void Basic_block::reset_pred_succ_dep(){
 }
 
 /* affiche les instructions successeurs des instructions du block */
-/* permet de visualiser les successeurs et impliciement les pr�d�cesseurs ! */ 
- 
+/* permet de visualiser les successeurs et impliciement les pr�d�cesseurs ! */
+
 void Basic_block::show_succ_dep(){
   if (!dep_done) return;
   cout << "Affichage dependance des instructions du BB " << get_index() << endl;
@@ -439,57 +439,63 @@ void Basic_block::show_succ_dep(){
 /* calcul le nb de cycles pour executer le BB, on suppose qu'une instruction peut sortir du pipeline � chaque cycle, il faut trouver les cycles de gel induit par les d�pendances */
 
 int Basic_block::nb_cycles(){
-  
+
   comput_pred_succ_dep(); // besoin d'avoir les d�pendances entre instruction
 
   /* tableau ci-dessous utile pour savoir pour chaque instruction quand elle sort pour en d�duire les cycles qu'elle peut induire avec les instructions qui en d�pendent, initialisation � -1  */
-   vector<int> inst_cycle(get_nb_inst()); 
+   vector<int> inst_cycle(get_nb_inst());
    for (int i=0; i< get_nb_inst(); i++ ){
      inst_cycle[i] = -1;
    }
-   
-   Instruction *ic=get_first_instruction();
-   int exect = 0;  
-   while(ic){   
 
-     // A REMPLIR 
-  
+   int exect = 0;
+   int cycle;
 
 
-     
-    
-     //FIN A REMPLIR 
-	#ifdef DEBUG
-		  cout << endl << "inst " << ic -> get_index() << " " << ic-> get_content () << " cycle "<<  inst_cycle[ic->get_index()] ;
-	#endif
-		 ic = ic->get_next();
+   Instruction* ic;
+
+   inst_cycle[0] = 1; // 1ere instr sort au cycle 1.
+
+   // TODO change for to while !
+   for (int i = 1; i < get_nb_inst(); i++) {
+         ic = get_instruction_at_index(i);
+         cycle = inst_cycle[i-1] + 1;
+
+         for (int j = 0; j < ic->get_nb_pred(); j++) {
+           Instruction *prev = ic->get_pred_dep(j)->inst;
+           if (ic->is_dep_WAR(prev) || ic->is_dep_RAW(prev)) {
+             cycle = max(cycle, inst_cycle[prev->get_index()] + delai(ic->get_type(), prev->get_type()));
+           }
+         }
+         inst_cycle[i] = cycle;
    }
 
 	#ifdef DEBUG
-		cout << endl;
+		cout << "********************************* nb_cycle : " << cycle << endl;
 	#endif
+
+	// jai 11 cycle pour l'exemple du td, dans le td c'est 10 donc a vérifier si ta pas un + 1 quelque part
    return exect;
-   
-  
+
+
 }
 
 
-
-/* 
-calcule DEF et USE pour l'analyse de registre vivant 
+/*
+calcule DEF et USE pour l'analyse de registre vivant
 � la fin on doit avoir
  USE[i] vaut 1 si $i est utilis� (lu) dans le bloc avant d'�tre potentiellement d�fini dans le bloc
- DEF[i] vaut 1 si $i est d�fini (�crit) dans le bloc 
-ne pas oublier les conventions d'appel : les registres $4, $5, $6, $7 peuvent contenir des param�tres (du 1er au 4eme les autres sont sur la pile) avant un appel de fonctions, au retour d'une fonction $2 a �t� �crit car il contient la valeur de retour (sauf si on rend void). 
+ DEF[i] vaut 1 si $i est d�fini (�crit) dans le bloc
+ne pas oublier les conventions d'appel : les registres $4, $5, $6, $7 peuvent contenir des param�tres (du 1er au 4eme les autres sont sur la pile) avant un appel de fonctions, au retour d'une fonction $2 a �t� �crit car il contient la valeur de retour (sauf si on rend void).
 Un appel de fonction (call) �crit aussi l'adresse de retour dans $31 donc d�finit implicitement $31.
 
 ******************/
 
 void Basic_block::compute_use_def(void){
   if (use_def_done) return;
- 
+
   /* A REMPLIR */
- 
+
 
 
   /* FIN A REMPLIR */
@@ -502,29 +508,29 @@ void Basic_block::show_use_def(void){
   cout << "USE : " ;
   for(int i=0; i<NB_REG; i++){
       if (Use[i])
-      cout << "$"<< i << " "; 
+      cout << "$"<< i << " ";
   }
   cout << endl;
   cout << "DEF : " ;
   for(int i=0; i<NB_REG; i++){
       if (Def[i])
-	cout << "$"<< i << " "; 
+	cout << "$"<< i << " ";
     }
   cout << endl;
   return;
 }
 
-/**** compute_def_liveout 
+/**** compute_def_liveout
 � la fin de la fonction on doit avoir
 DefLiveOut[i] vaut l'index de l'instruction du bloc qui d�finit $i si $i vivant en sortie seulement
 Si $i est d�fini plusieurs fois c'est l'instruction avec l'index le plus grand
 *****/
 void Basic_block::compute_def_liveout(){
-  
- 
+
+
   /* A REMPLIR */
 
- 
+
 
 
   /* FIN A REMPLIR */
@@ -537,39 +543,39 @@ void Basic_block::show_def_liveout(){
   cout << "DEF LIVE OUT: " ;
   for(int i=0; i<NB_REG; i++){
     if (DefLiveOut[i] != -1)
-      cout << "$"<< i << " definit par l'instruction i" << DefLiveOut[i] << endl; 
+      cout << "$"<< i << " definit par l'instruction i" << DefLiveOut[i] << endl;
   }
   cout << endl;
   return;
 }
 
 /**** renomme les registres renommables : ceux qui sont d�finis et utilis�s dans le bloc et dont la d�finition n'est pas vivante en sortie
-Utilise comme registres disponibles ceux dont le num�ro est dans la liste param�tre 
+Utilise comme registres disponibles ceux dont le num�ro est dans la liste param�tre
 *****/
 
 void Basic_block::reg_rename(list<int> *frees){
   compute_def_liveout();   // definition vivantes en sortie necessaires � connaitre
- 
+
   /* A REMPLIR */
 
 
 
   /* FIN A REMPLIR */
 
- 
+
 }
 
 
 
 /**** renomme les registres renommables : ceux qui sont d�finis et utilis�s dans le bloc et dont la d�finition n'est pas vivante en sortie
-Utilise comme registres disponibles ceux dont le num�ro est dans la liste param�tre 
+Utilise comme registres disponibles ceux dont le num�ro est dans la liste param�tre
 *****/
 void Basic_block::reg_rename(){
- 
+
   compute_def_liveout();
- 
+
   list<int> free_regs;
- 
+
    /* A REMPLIR */
 
 
@@ -588,7 +594,7 @@ void Basic_block::apply_scheduling(list <Node_dfg*> *new_order){
       cout<<"wrong bb : cannot apply"<<endl;
       return;
    }
-   
+
    while(!n->isInst()){
      prevn=n;
      n=n->get_next();
@@ -597,13 +603,13 @@ void Basic_block::apply_scheduling(list <Node_dfg*> *new_order){
        return;
      }
    }
-   
+
    //y'a des instructions, on sait pas si c'est le bon BB, mais on va supposer que oui
    inst->set_index(0);
    inst->set_prev(nullptr);
    _firstInst = inst;
    n = inst;
-   
+
    if(prevn){
      prevn->set_next(n);
      n->set_prev(prevn);
@@ -615,7 +621,7 @@ void Basic_block::apply_scheduling(list <Node_dfg*> *new_order){
    int i;
    it++;
    for(i=1; it!=new_order->end(); it++, i++){
-     inst->set_link_succ_pred((*it)->get_instruction());   
+     inst->set_link_succ_pred((*it)->get_instruction());
      inst=(*it)->get_instruction();
      inst->set_index(i);
      prevn = n;
@@ -640,7 +646,7 @@ void Basic_block::apply_scheduling(list <Node_dfg*> *new_order){
 void Basic_block::test(){
    cout << "test du BB " << get_index() << endl;
    display();
-  
+
    cout <<  "nb de predecesseurs : " << get_nb_pred() << endl ;
    int nbpred = (int) get_nb_pred();
    for (int i = 0; i < nbpred; i++){
@@ -650,7 +656,7 @@ void Basic_block::test(){
    cout << endl;
    int nbsucc = (int) get_nb_succ();
    cout << "nb de successeurs : " << get_nb_succ() << endl;
-   
+
    for (int i = 0; i < nbsucc; i++){
      if (get_successor(i) != nullptr)
        cout << " succ " << i  << " : BB" <<  get_successor(i)-> get_index() << endl ;
